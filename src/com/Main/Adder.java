7176bfd601715c2d25ccb66c302e9ac01a5e734f
Nathaniel <nathanielzyc@gmail.com>
2016-04-11 21:38:08 +0800

    Polish up code in Adder Class
    SLAP
    Java Standard Check

--- src/com/Main/Adder.java
+++ src/com/Main/Adder.java
@@ -1,27 +1,26 @@
 package com.Main;
-//@@author A0100111R
 import java.util.ArrayList;
 import java.util.logging.Level;
+//@@author A0100111R
 
 public class Adder implements Commander{
 	private Task newTask = new Task();
-	ArrayList<Task> TaskList = new ArrayList<Task>();
-	private static int taskIDMax = 0;
+	ArrayList<Task> taskList = new ArrayList<Task>();
+	private static int taskIdMaxLimit = 0;
 	
 
 	public static int getTaskIDMax() {
-		return taskIDMax;
+		return taskIdMaxLimit;
 	}
 
 	public static void setTaskIDMax(int taskIDMax) {
-		Adder.taskIDMax = taskIDMax;
+		Adder.taskIdMaxLimit = taskIDMax;
 	}
 
-	public Adder(String[] parsedUserInput, ArrayList<Task> TaskList) {
-		//The element of the string array will be in 
+	public Adder(String[] parsedUserInput, ArrayList<Task> originalTaskList) {
+		//The element of the string array will be 
 		//0.taskName, 1.location 2.date 3.start 4.end 5.tag 6.notification
-		//int taskListSize = TaskList.size();
-		this.TaskList = TaskList;
+		this.taskList = originalTaskList;
 		try {
 			newTask.setTask(parsedUserInput[0]);
 			newTask.setLocation(parsedUserInput[1]);
@@ -31,11 +30,9 @@ public class Adder implements Commander{
 			newTask.setTag(parsedUserInput[5]);
 			newTask.setNotification(parsedUserInput[6]);
 			newTask.setCalendar();
-			newTask.setTaskID(taskIDMax+1);
+			newTask.setTaskID(taskIdMaxLimit+1);
 			newTask.determineTaskType();
 		} catch (NullPointerException e) {
-			
-			//e.printStackTrace();
 			System.out.println("The Task is null. " + e.toString());
 		}
 	}
@@ -49,33 +46,48 @@ public class Adder implements Commander{
 			return "Task NOT added successfully due to invalid date!";
 		}
 		
-			TaskList.add(newTask);
-			taskIDMax++;
-			Displayer updateGUI = new Displayer(TaskList);
-			updateGUI.execute();
-			
-			Processor.setLastCommanderInst(this);
+			taskList.add(newTask);
+			updateAfterSuccessfulExecution();
 			
 			String taskType = newTask.getTaskType().toString();
 			return "Task added successfully"+ "with type being: "+ taskType;
-
+			
 			//ProcessorLogger.log(Level.WARNING, "task not added successfully", e);
 		
+	}
+
+	/**
+	 * Update the following:
+	 * task ID limit, GUI display panel and undo track.
+	 */
+	private void updateAfterSuccessfulExecution() {
+		taskIdMaxLimit++;
+		updateGui();			
+		Processor.setLastCommanderInst(this);
 	}
 
 	@Override
 	public String undo() {
 		ConverterToString taskConverter = new ConverterToString(newTask);
-		String feedback = taskConverter.convert();
+		String taskFeedBackString = taskConverter.convert();
 		
-		if(TaskList.remove(newTask)){
-			Processor.setLastCommanderInst(null);//Undoing add is designed for no chance of redoing.
-			Displayer updateGUI = new Displayer(TaskList);
-			updateGUI.execute();
-			return "The following task is removed:"+System.lineSeparator()+feedback;		
+		if(taskList.remove(newTask)){
+			//Undoing adding is designed for no chance of redoing.
+			Processor.setLastCommanderInst(null);
+			updateGui();
+			return "The following task is removed:"
+			+System.lineSeparator()+taskFeedBackString;		
 		}
 		else
 			return "The undo to remove last task added is unsuccessful.";
+	}
+
+	/**
+	 * 
+	 */
+	private void updateGui() {
+		Displayer guiUpdater = new Displayer(taskList);
+		guiUpdater.execute();
 	}
 
 }
