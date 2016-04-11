8e7f92ad1b647409eb6939bb84a480b5cdeb57bb
Nathaniel <nathanielzyc@gmail.com>
2016-04-11 21:51:51 +0800

    Polish up code in Deleter Class
    Java code standard check
    SLAP

--- src/com/Main/Deleter.java
+++ src/com/Main/Deleter.java
@@ -3,67 +3,79 @@ package com.Main;
 import java.util.ArrayList;
 
 //@@author A0100111R
-public class Deleter implements Commander{
+public class Deleter implements Commander {
 	private int taskID;
 	private int taskIndex;
-	private ArrayList<Task> TaskList;
+	private ArrayList<Task> taskList = null;
 	private Task taskDeleted = null;
-	
-	public Deleter(String[] parsedUserInput, ArrayList<Task> TaskList){
-		//The first element in the array is the taskID to be deleted
+
+	public Deleter(String[] parsedUserInput, ArrayList<Task> TaskList) {
+		// The first element in the array is the taskID to be deleted
 		taskID = Integer.parseInt(parsedUserInput[0]);
-		this.TaskList = TaskList;
+		this.taskList = TaskList;
 	}
+
 	@Override
 	public String execute() {
 		taskIndex = SearchTaskByID(taskID);
-		if(taskIndex < 0){
+		if (taskIndex < 0) {
 			return "The taskID entered is invalid, please re-enter.";
-		}else{
-			taskDeleted = TaskList.remove(taskIndex);
-			
-			Displayer updateGUI = new Displayer(TaskList);
-			updateGUI.execute();
-			
-			Processor.setLastCommanderInst(this);
-			
-			ConverterToString taskDeletedConversion = new ConverterToString(taskDeleted);
-			String taskDeletedString = taskDeletedConversion.convert();
-			String feedback = "Task deleted successfully:"+System.lineSeparator()+taskDeletedString;
+		} else {
+			taskDeleted = taskList.remove(taskIndex);
+			updateGui();
+			updateUndoTracker();
+
+			String taskDeletedString = convertTaskToStringForFeedback();
+			String feedback = "Task deleted successfully:" 
+			+ System.lineSeparator() + taskDeletedString;
 			return feedback;
 		}
-	}	
-/*		
-		try{
-		//assert(taskID >=0);
-		//taskDeleted = TaskList.get(taskID-1).getTaskName() + " on " + TaskList.get(taskID-1).getDate();
-			
-		} catch (Exception e){
-			//e.printStackTrace();
-			return "Task deleted unsuccessfully";
-		}
-*/		
+	}
 
-	
+	/**
+	 * @return a string
+	 */
+	private String convertTaskToStringForFeedback() {
+		ConverterToString taskDeletedConversion = new ConverterToString(taskDeleted);
+		String taskDeletedString = taskDeletedConversion.convert();
+		return taskDeletedString;
+	}
+
+	/**
+	 * Update the user action tracker for future undo action
+	 */
+	private void updateUndoTracker() {
+		Processor.setLastCommanderInst(this);
+	}
+
+	// Search the task with specified ID
 	private int SearchTaskByID(int DesiredTaskID) {
-		for (int i = 0; i < TaskList.size(); i++){
-			if(DesiredTaskID == TaskList.get(i).getTaskID()){
+		for (int i = 0; i < taskList.size(); i++) {
+			if (DesiredTaskID == taskList.get(i).getTaskID()) {
 				return i;
 			}
 		}
-		return -1; //-1 for the case of invalid TaskId which can't be found the match.
+		// -1 for the case of invalid TaskId which can't be found 
+		return -1; 					
 	}
+
 	@Override
 	public String undo() {
-		ConverterToString taskConverter = new ConverterToString(taskDeleted);
-		String feedback = taskConverter.convert();
-		TaskList.add(taskDeleted);
-		
-		Displayer updateGUI = new Displayer(TaskList);
+		String feedback = convertTaskToStringForFeedback();
+		taskList.add(taskDeleted);
+		updateGui();
+		// Undoing delete is designed for no chance of redoing.
+		Processor.setLastCommanderInst(null);		
+		return "The following task you deleted is added in again: " 
+				+ System.lineSeparator() + feedback;
+	}
+
+	/**
+	 * update GUI display panel
+	 */
+	private void updateGui() {
+		Displayer updateGUI = new Displayer(taskList);
 		updateGUI.execute();
-		
-		Processor.setLastCommanderInst(null);//Undoing delete is designed for no chance of redoing.
-		return "The following task you deleted is added in again: "+System.lineSeparator()+feedback;
 	}
 
 }
