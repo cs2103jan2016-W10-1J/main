982ad94c9250a603f3c3ae7da055844124de3032
Nathaniel <nathanielzyc@gmail.com>
2016-04-11 22:47:00 +0800

    Polish code in Commander Class

--- src/com/Main/Commander.java
+++ src/com/Main/Commander.java
@@ -1,7 +1,13 @@
+//@@author A0100111R
 package com.Main;
-
-import java.util.ArrayList;
-
+/*
+ * This Commander interface is created to facilitate that 
+ * in the Processor object, the Parser object can pass in 
+ * any objects of sub-class of Commander Class, which can 
+ * be received by the Processor object without knowing its
+ * specific class type. This is an implementation of high-level
+ * OOP so as to apply de-coupling and enhance cohesiveness.
+ */
 public interface Commander {
 	String execute();
 	String undo();
